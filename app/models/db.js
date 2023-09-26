892b7343bbe3379a888ec42b6dba44368e7117e4
waritsara3844 <70258816+waritsara3844@users.noreply.github.com>
2023-09-26 10:22:21 +0700

    Update db.js

--- app/models/db.js
+++ app/models/db.js
@@ -1,13 +1,20 @@
 const mysql = require("mysql2");
 const dbConfig = require("../config/db.config");
 const connection = mysql.createConnection({
-    host: dbConfig.HOST,
-    user: dbConfig.USER,
-    password: dbConfig.PASSWORD,
-    database: dbConfig.DB
+  host: dbConfig.HOST,
+  user: dbConfig.USER,
+  password: dbConfig.PASSWORD,
+  database: dbConfig.DB,
 });
-connection.connect((error)=>{
-    if(error) console.log("MYSQL connection: " + error);
-    else console.log("Successfully connected to the database");
+connection.connect((error) => {
+  if (error) console.log("MYSQL connection: " + error);
+  else console.log("Successfully connected to the database");
 });
+
+setInterval(function () {
+  connection.ping((err) => {
+    if (err) throw err;
+  });
+}, 10000);
+
 module.exports = connection;
